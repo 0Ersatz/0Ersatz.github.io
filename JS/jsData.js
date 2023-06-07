afd64978aa57374902b3a79fced475594f262f8d
0Ersatz <64142140+0Ersatz@users.noreply.github.com>
2023-06-07 22:58:08 +0300

    Update jsData.js

--- JS/jsData.js
+++ JS/jsData.js
@@ -1,18 +1,21 @@
+async function myFunction() {
+    try {
+        const response = await fetch('https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1');
+        const data = await response.json();
+        const el = document.getElementById('demo');
+        el.innerHTML = '';
+
+        for (const [key, value] of Object.entries(data[0])) {
+            const newEl = document.createElement('li');
+            newEl.appendChild(document.createTextNode(`${key}: ${value}`));
+            el.appendChild(newEl);
+            await sleep(0);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-function getDataFromApi() {
-        (async () => {
-          let response = await fetch('https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1%27);
-          let el = document.getElementById('demo')
-          el.innerHTML = ""
-          let text = await response.text(); 
-          for (const [key, value] of Object.entries(JSON.parse(text)[0])) {
-            const newEl = document.createElement("li")
-            newEl.appendChild(document.createTextNode(${key}: ${value}))
-            el.appendChild(newEl)
-            await sleep(100)
-          }
-        })()
-      }
